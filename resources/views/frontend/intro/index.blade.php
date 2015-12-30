8b3956de52510a78ac94aa703abfb7661fdd1134
Lê Vĩnh Thiện <ohmygodvt95@gmail.com>
2015-12-30 21:53:57 +0700

    update

--- resources/views/frontend/intro/index.blade.php
+++ resources/views/frontend/intro/index.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LengKeng</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
@@ -10,14 +12,10 @@
     <link rel="stylesheet" href="{{ asset('css/jquery.kenburnsy.css') }}">
     <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/frontend/intro/style.css') }}">
-    <link href='https://fonts.googleapis.com/css?family=Raleway:400,700,800|Oswald:400,700|Dosis:400,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,700,800|Oswald:400,500,600,700|Dosis:400,300' rel='stylesheet' type='text/css'>
 </head>
 <body>
     {{-- BG --}}
-    {{-- <div id="owl-demo" class="bg owl-carousel owl-theme">
-        <div class="item"><div class="bg-img-1"></div></div>
-        <div class="item"><div class="bg-img-2"></div></div>
-    </div> --}}
     <div id="bg" class="bg">
         <img src="http://www.11-76.com/themes/city/img/background/4.jpg" alt="">
         <img src="http://www.11-76.com/themes/city/img/background/1.jpg" alt="">
@@ -26,12 +24,12 @@
     <div class="menu">
         <i class="fa fa-fw fa-bars fa-4x"></i>
     </div>
-    <section id="intro">
+    <section id="intro" >
         <div class="intro">
             <div class="logo"></div>
             <div class="content">
                 <h3 class="text-center">WHO I AM?</h3>
-                <p class="text-center">LVT</p>
+                <h1 class="text-center"><span>LVT</span></h1>
                 <div class="content-box center-block">
                     <div id="owl-heading" class="owl-carousel owl-theme">
                         <div class="item"><div class="text-center">LE VINH THIEN</div></div>
@@ -39,6 +37,11 @@
                         <div class="item"><div class="text-center">HUST - K58</div></div>
                     </div>
                 </div>
+                <h1 class="text-center"><span class="heading-bottom">LVT</span></h1>
+            </div>
+            <div class="hr center-block"></div>
+            <div class="center-block next">
+                <i class="fa fa-fw fa-angle-double-down"></i>
             </div>
         </div>
     </section>
