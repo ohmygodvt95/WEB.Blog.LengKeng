0bf60da6566b8b7c87a477cc7f609fdbcac5e8f6
Lê Vĩnh Thiện <ohmygodvt95@gmail.com>
2016-01-03 18:01:22 +0700

    update seo

--- resources/views/frontend/intro/index.blade.php
+++ resources/views/frontend/intro/index.blade.php
@@ -4,7 +4,29 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Le Vinh Thien - Introduction</title>
+    <meta http-equiv="content-language" content="vi" />
+    <meta name="robots" content="noodp,index,follow" />
+    <meta name="description" content="HOME | Welcome to my personal website - I'm Le Vinh Thien(LVT), student in  Hanoi University of Science and Technology - Viet Nam. I'm a Software Engineer." />
+    <meta name="keywords" content="Le Vinh Thien, LVT, LengKeng, vinhthien, Home, vinhthien.name.vn, student, software engineer, university, personal website" />
+    {{-- fb --}}
+    <meta property="og:title" content="HOME | Le Vinh Thien - Personal website" />
+    <meta property="og:locale" content="vi_VN" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{$_SERVER['SERVER_NAME']}}" />
+    <meta property="og:image" content="{{ asset('images/bg/bg2.jpg') }}" />
+    <meta property="og:description" content="HOME | Welcome to my personal website - I'm Le Vinh Thien(LVT), student in  Hanoi University of Science and Technology - Viet Nam. I'm a Software Engineer." />
+    <meta property="og:site_name" content="HOME | Le Vinh Thien - Personal website" />
+    <meta property="fb:admins" content="ohmygodvt95" />
+    {{-- twitter --}}
+    <meta name="twitter:card" content="HOME | Welcome to my personal website - I'm Le Vinh Thien">
+    <meta name="twitter:site" content="@ohmygodvt95">
+    <meta name="twitter:title" content="HOME | Le Vinh Thien - Personal website">
+    <meta name="twitter:description" content="HOME | Welcome to my personal website - I'm Le Vinh Thien(LVT), student in  Hanoi University of Science and Technology - Viet Nam. I'm a Software Engineer.">
+    <meta name="twitter:creator" content="@ohmygodvt95">
+    {{-- LINK --}}
+    <link rel="canonical" href="{{$_SERVER['SERVER_NAME']}}"/>
+    <title>HOME | Le Vinh Thien - Personal website</title>
+    <link href="{{ asset('images/favicon.png') }}" rel="shortcut icon" type="image/x-icon" />
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/preload.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/owl.carousel.css') }}">
@@ -14,7 +36,6 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/intro/style.css') }}">
     <link href='https://fonts.googleapis.com/css?family=Raleway:100,400,700,800|Oswald:400,500,600,700|Dosis:400,300,700' rel='stylesheet' type='text/css'>
-    <!-- google fonts end -->
     <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
@@ -44,6 +65,7 @@
         <img src="{{ asset('images/bg/bg3.jpg') }}">
         <img src="{{ asset('images/bg/bg1.jpg') }}">
     </div>
+    <h1 class="hidden">HOME | Welcome to my personal website</h1>
     {{-- SECTION INTRO --}}
     <div class="menu">
         <i class="fa fa-bars fa-4x" status = "close"></i>
@@ -68,7 +90,7 @@
             <div class="logo"></div>
             <div class="content">
                 <h3 class="text-center">.: WELCOME TO ME :.</h3>
-                <h1 class="text-center"><span>LVT</span></h1>
+                <h1 class="text-center" title="Le Vinh Thien"><span>LVT</span></h1>
                 <div class="content-box center-block">
                     <div id="owl-heading" class="owl-carousel owl-theme">
                         <div class="item"><div class="text-center">LE VINH THIEN</div></div>
@@ -76,7 +98,7 @@
                         <div class="item"><div class="text-center">HUST - K58</div></div>
                     </div>
                 </div>
-                <h1 class="text-center"><span class="heading-bottom">LVT</span></h1>
+                <h1 class="text-center" title="Lê Vĩnh Thiện"><span class="heading-bottom">LVT</span></h1>
             </div>
             <div class="hr center-block"></div>
             <div class="center-block next">
@@ -88,61 +110,22 @@
     {{-- SECTION ABOUT --}}
     <section id="about" class="disabled" state="disabled">
         <div class="container">
-            <h1 class="text-center heading">About</h1>
+            <h2 class="text-center heading">About</h2>
             <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
         </div>
     </section>
     {{-- SECTION PROJECT --}}
     <section id="project" class="disabled" state="disabled">
         <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
+            <h2 class="text-center heading">PROJECT</h2>
             <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
         </div>
         <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
+            <h2 class="text-center heading">PROJECT</h2>
             <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
         </div>
         <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
-            <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
-        </div>
-        <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
-            <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
-        </div>
-        <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
-            <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
-        </div>
-        <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
-            <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
-        </div>
-        <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
-            <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
-        </div>
-        <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
-            <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
-        </div>
-        <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
-            <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
-        </div>
-        <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
-            <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
-        </div>
-        <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
-            <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
-        </div>
-        <div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
-            <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
-        </div><div class="container">
-            <h1 class="text-center heading">PROJECT</h1>
+            <h2 class="text-center heading">PROJECT</h2>
             <h4 class="text-center sub-heading">Lorem ipsum Irure occaecat laboris dolore minim ut.</h4>
         </div>
 
@@ -227,6 +210,16 @@
         $(window).load(function() {
             $("#loading").fadeOut(1000);
         });
+    </script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-71954602-1', 'auto');
+      ga('send', 'pageview');
+
     </script>
 </body>
 </html>
